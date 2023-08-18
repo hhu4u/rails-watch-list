f60fbcdaffe8f694675572aad3cdc230da8a808e
herve hublet <hervehublet@hotmail.com>
2023-08-18 11:59:14 +0200

    bookmarks controller create to finish

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,28 @@
+class BookmarksController < ApplicationController
+  # def index
+  #   @bookmarks = Bookmark.all
+  # end
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to
+      # show de list
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
